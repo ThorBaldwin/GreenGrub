8b731ff61cb9b972ad5973196eee34d2bac714eb
Your Name <you@example.com>
2024-06-03 21:35:48 -0700

    first commit for GreeGrub

---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from "/components/ui/button"
+
+export default function Navbar() {
+  return (
+    <div className='w-3/4 m-auto bg-transparent h-20 flex justify-between items-center'>
+        <div>
+            <Link href="/">
+                <Image 
+                src="/earth.svg"
+                alt="logo"
+                width={50}
+                height={150}
+                />
+            </Link>
+        </div>
+        <div>
+            <Link 
+            href={"/"}
+            >
+                <Button 
+                size="sm"
+                variant="outline"
+                >Log in</Button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
