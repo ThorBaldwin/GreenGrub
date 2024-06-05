3951acc079b78915be72d70416bab20954966799
Your Name <you@example.com>
2024-06-05 15:31:20 -0700

    placeholders

--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -3,8 +3,9 @@
 
 function page() {
   return (
-    <div className="w-3/4 m-auto flex justify-center h-72 items-center">
-        <h1 className="text-5xl font-black">Oh would you look at that 🫠 <br /> This page is not yet finished 🥲</h1>
+    <div className="w-3/4 m-auto flex flex-col justify-center min-h-72 text-5xl font-black items-center max-sm:gap-10">
+        <h1>Oh would you look at that 🫠</h1>
+        <h1>This page is not yet finished 🥲</h1>
     </div>  )
 }
 
