63e0786701f19c76e7281735fdf1e104593b1b85
Your Name <you@example.com>
2024-06-05 15:10:53 -0700

    placeholder responsive

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,7 @@
 
 function page() {
   return (
-    <div className="w-3/4 m-auto flex justify-center">
+    <div className="w-3/4 m-auto flex justify-center h-72 items-center">
         <h1 className="text-5xl font-black">This page is not yet finished 🥲</h1>
     </div>
   )
--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -3,7 +3,7 @@
 
 function page() {
   return (
-    <div className="w-3/4 m-auto flex justify-center">
+    <div className="w-3/4 m-auto flex justify-center h-72 items-center">
         <h1 className="text-5xl font-black">Oh would you look at that 🫠 <br /> This page is not yet finished 🥲</h1>
     </div>  )
 }
